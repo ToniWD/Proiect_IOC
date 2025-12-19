8cdd3e2642b6ba673c6aab60caf2fb8ff18bc2de
danhepai <socialmedia.dangaspar@gmail.com>
2025-12-20 00:19:10 +0200

    Progress on level

--- SavePoint-Momo/Assets/Scripts/Levels/Level 9/Level9.cs
+++ SavePoint-Momo/Assets/Scripts/Levels/Level 9/Level9.cs
@@ -2,16 +2,19 @@ using System;
 using System.Collections;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 public class Level9 : MonoBehaviour
 {
-
     public AudioManager audioManager;
     public GameObject greenLight;
+    public GameObject redLight;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject stayButton;
+    public GameObject goButton;
 
-    // TODO: hit Adi for icons for buttons (Trecem / Stam);
+    private float lastHoverTime; // Stores the timestamp of the last hover
+    public float hoverCooldown = 1f; // Delay in seconds (adjust this as you like)
     void Start()
     {
         audioManager = GameObject.Find("AudioPlayer").GetComponent<AudioManager>();
@@ -21,29 +24,73 @@ public class Level9 : MonoBehaviour
         ));
     }
 
+    void Update() { }
+
     void startGame()
     {
-        greenLight.SetActive(true);
+        redLight.SetActive(true);
+        StartCoroutine(audioManager.PlayAudioList(new List<string> { "01-instructiuni" },
+            this.showButtons
+        ));
     }
 
-    // Update is called once per frame
-    void Update()
+    void showButtons()
     {
-
+        stayButton.SetActive(true);
+        goButton.SetActive(true);
     }
 
-    public void clickTrecem()
+    public void playGoButtonSound()
     {
-        Debug.Log("Trecem");
-
-        if (greenLight.activeSelf)
+        if (Time.time > lastHoverTime + hoverCooldown)
         {
-            Debug.Log("Bravo");
+            StartCoroutine(audioManager.PlayAudioList(new List<string> { "99-trecem" }));
+            lastHoverTime = Time.time;
         }
     }
 
-    public void clickStam()
+    public void playStayButtonSound()
     {
-        Debug.Log("Stam");
+        StartCoroutine(audioManager.PlayAudioList(new List<string> { "98-asteptam" }));
+    }
+
+    public void clickStayButton()
+    {
+        if (redLight.activeSelf)
+        {
+            StartCoroutine(audioManager.PlayAudioList(new List<string> { "02-rosu-corect" }));
+        }
+        else
+        {
+            StartCoroutine(audioManager.PlayAudioList(new List<string> { "03-rosu-gresit" }));
+        }
+    }
+
+    public void clickGoButton()
+    {
+
+        if (greenLight.activeSelf)
+        {
+            StartCoroutine(audioManager.PlayAudioList(new List<string> { "04-verde-corect" }));
+        }
+        else
+        {
+            // Red is active. So it's a NO GO
+            playIncorrectGoSound();
+        }
+    }
+
+    void playIncorrectGoSound()
+    {
+        List<string> audios = new List<string> {
+            "01-nu-trecem-pe-rosu",
+            "02-nu-trecem-pe-rosu",
+            "03-nu-trecem-pe-rosu",
+            "04-nu-trecem-pe-rosu"
+        };
+        int randomIndex = UnityEngine.Random.Range(0, audios.Count);
+        string selectedAudio = audios[randomIndex];
+
+        StartCoroutine(audioManager.PlayAudioList(new List<string> { selectedAudio }));
     }
 }
